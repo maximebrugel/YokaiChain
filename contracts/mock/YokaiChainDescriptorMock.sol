4492d879529088306142ba95569848ee60db19c8
axxedevv <maxime.brugel@gmail.com>
2021-10-20 12:40:43 +0200

    fix: new background proba

--- contracts/mock/YokaiChainDescriptorMock.sol
+++ contracts/mock/YokaiChainDescriptorMock.sol
@@ -15,7 +15,7 @@ contract YokaiChainDescriptorMock is IYokaiChainDescriptor {
     /// @dev Max value for defining probabilities
     uint256 internal constant MAX = 100000;
 
-    uint256[] internal BACKGROUND_ITEMS = [4000, 3400, 3080, 2750, 2400, 1900, 1200, 0];
+    uint256[] internal BACKGROUND_ITEMS = [3000, 2650, 2400, 2200, 2000, 1700, 1300, 0];
     uint256[] internal SKIN_ITEMS = [52000, 26000, 1000, 0];
     uint256[] internal NOSE_ITEMS = [75000, 55000, 39000, 26000, 15000, 5000, 100, 0];
     uint256[] internal MARK_ITEMS = [
